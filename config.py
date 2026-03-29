fc88386b20d00074bce3058aca1d17491f432fed
Giovanni Barbaro <vanni-96@hotmail.it>
2026-03-29 12:51:01 +0200

    Makes use of env variables

--- config.py
+++ config.py
@@ -1,27 +1,17 @@
-PORT = 443
+import os
+
+PORT = int(os.getenv("MTPROTO_PORT", 443))
 
 # name -> secret (32 hex chars)
-USERS = {
-    "tg":  "00000000000000000000000000000001",
-    # "tg2": "0123456789abcdef0123456789abcdef",
-}
+# USERS environment variable format: "user1:secret1,user2:secret2"
+users_env = os.getenv("MTPROTO_USERS", "tg:00000000000000000000000000000001")
+USERS = dict(user.split(":") for user in users_env.split(","))
 
 MODES = {
-    # Classic mode, easy to detect
-    "classic": False,
-
-    # Makes the proxy harder to detect
-    # Can be incompatible with very old clients
-    "secure": False,
-
-    # Makes the proxy even more hard to detect
-    # Can be incompatible with old clients
-    "tls": True
+    "classic": os.getenv("MTPROTO_MODE_CLASSIC", "False").lower() == "true",
+    "secure": os.getenv("MTPROTO_MODE_SECURE", "False").lower() == "true",
+    "tls": os.getenv("MTPROTO_MODE_TLS", "True").lower() == "true"
 }
 
-# The domain for TLS mode, bad clients are proxied there
-# Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
-
-# Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+TLS_DOMAIN = os.getenv("MTPROTO_TLS_DOMAIN", "www.google.com")
+AD_TAG = os.getenv("MTPROTO_AD_TAG", None)
